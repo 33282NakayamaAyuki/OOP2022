cbf3367a020dd222426a4bf2c6b3d5ed770aa2a1
33282歩季　中山 <33282@ojs.ac.jp>
2022-04-28 15:06:46 +0900

    メソッドの移動

--- Chapter02/SalesCalculator/Program.cs
+++ Chapter02/SalesCalculator/Program.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace SalesCalculator {
     class Program {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            SalesCounter sales = new SalesCounter(ReadSales("sales.csv"));
+            SalesCounter sales = new SalesCounter(SalesCounter.ReadSales("sales.csv"));
             Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
             foreach (KeyValuePair<String, int> obj in amountPerStore)
             {
@@ -17,25 +17,6 @@ namespace SalesCalculator {
             }
         
         }
-        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        static List<Sale> ReadSales(string filePath)
-        {
-            List<Sale> sales = new List<Sale>();
-            String[] lines = File.ReadAllLines(filePath);
-            foreach (String line in lines)
-            {
-                String[] items = line.Split(',');
-                Sale sale = new Sale
-                {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2])
-                };
-                sales.Add(sale);
-            }
-            
-
-            return sales;
-        }
+       
     }
 }
