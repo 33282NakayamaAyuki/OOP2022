01947eae551e435e50a7fe12a64679203c1c8df0
33282歩季　中山 <33282@ojs.ac.jp>
2022-05-10 13:49:07 +0900

    問題2.1.1　完成

---
+++ Chapter02/Exercise/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise {
+    class Song {
+
+        public String Title { get; set; }
+
+        public String ArtistName { get; set; }
+
+        public int Length { get; set; }
+
+    }
+    
+}
