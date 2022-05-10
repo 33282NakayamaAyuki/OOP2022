b7f531c0b519083c315404db1bd40ea4079806ab
33282歩季　中山 <33282@ojs.ac.jp>
2022-05-10 13:49:36 +0900

    問題2.1.2 完成

--- Chapter02/Exercise/Song.cs
+++ Chapter02/Exercise/Song.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 namespace Exercise {
     class Song {
 
+        public Song(String Title, String ArtistName, int Length)
+        {
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Length;
+        }
+
         public String Title { get; set; }
 
         public String ArtistName { get; set; }
