d2e940e74d162f67948d55f2a0a7e1ab114c20a8
33282歩季　中山 <33282@ojs.ac.jp>
2022-05-10 15:20:14 +0900

    問題2.1.3 完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -6,8 +6,35 @@ using System.Threading.Tasks;
 
 namespace Exercise {
     class Program {
-        static void Main(string[] args)
-        {
+
+        static void Main(String[] args) {
+
+            var song1 = new Song("a", "a", 9);
+            var song2 = new Song("a", "a", 9);
+            var song3 = new Song("a", "a", 9);
+
+            var songs = new List<Song>();
+            songs.Add(song1);
+            songs.Add(song2);
+            songs.Add(song3);
+
+            //var songs = new Song[] {
+            //    new Song("a", "a", 9),
+            //    new Song("a", "a", 9),
+            //    new Song("a", "a", 9),
+            //    new Song("a", "a", 9),
+            //    new Song("a", "a", 9),
+            //};
+
+           // PrintSongs(songs);
         }
+        //private static void PrintSongs(List<Song> songs)
+        //{
+        //    foreach (var song in songs)
+        //    {
+
+        //        Console.WriteLine("[{0}]", (",", songs));
+        //    }
+        //}
     }
 }
--- Chapter02/Exercise/Song.cs
+++ Chapter02/Exercise/Song.cs
@@ -7,19 +7,21 @@ using System.Threading.Tasks;
 namespace Exercise {
     class Song {
 
+        //曲のタイトル
+        public String Title { get; set; }
+
+        //アーティスト名
+        public String ArtistName { get; set; }
+
+        //演奏時間
+        public int Length { get; set; }
+
+        //コンストラクタ
         public Song(String Title, String ArtistName, int Length)
         {
             this.Title = Title;
             this.ArtistName = ArtistName;
             this.Length = Length;
         }
-
-        public String Title { get; set; }
-
-        public String ArtistName { get; set; }
-
-        public int Length { get; set; }
-
     }
-    
 }
