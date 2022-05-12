814cd84a53b154f98c2d504842b8708c736c404b
33282歩季　中山 <33282@ojs.ac.jp>
2022-05-12 14:23:14 +0900

    問題2-2　完成

---
+++ Chapter02/Exercise02/InchConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    public static class InchConverter {
+        private const double ratio = 0.0254;
+
+        public static double ToMeter(double inch)
+        {
+            return ratio * inch;
+        }
+    }
+}
--- Chapter02/Exercise02/Program.cs
+++ Chapter02/Exercise02/Program.cs
@@ -5,9 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise02 {
-    class Program {
+    public class Program {
         static void Main(string[] args)
         {
+            PrintMeterToInchList(1, 10);
+        }
+
+        private static void PrintMeterToInchList(int start, int stop)
+        {
+            //FeetConverter feetconverter = new FeetConverter();
+            for (int inch = start; inch <= stop; inch++)
+            {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0} inch = {1:0.0000} m", inch, meter);
+            }
         }
     }
 }
