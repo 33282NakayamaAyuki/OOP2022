534fab23209e6ab47218902e7019cc327b9072fc
33282歩季　中山 <33282@ojs.ac.jp>
2022-05-27 15:33:50 +0900

    問題　5.3.3、5.3.4，5.3.5　完成

--- Chapter05/Exercise03/Program.cs
+++ Chapter05/Exercise03/Program.cs
@@ -26,27 +26,42 @@ namespace Exercise03 {
 
         private static void Exercise3_1(string text) {
 
-            
+            int spaces = text.Count(c => c == ' ');
+            Console.WriteLine("空白数:{0}", spaces);
         }
 
         private static void Exercise3_2(string text) {
 
-
+            var result = text.Replace("big","small");
+            Console.WriteLine(result);
         }
 
         private static void Exercise3_3(string text) {
 
-
+            int words = text.Split(' ').Length;
+            
+            Console.WriteLine("単語の個数:{0}", words);
         }
 
         private static void Exercise3_4(string text) {
 
+            string[] words = text.Split(' ');
 
+            foreach (var word in words.Where(word => word.Length <= 4))
+            {
+                Console.WriteLine(word);
+            }
         }
 
         private static void Exercise3_5(string text) {
-
-
+            String[] words = text.Split(' ');
+            var sb = new StringBuilder();
+            foreach (var word in words)
+            {
+                sb.Append(word + ' ');
+            }
+            var texts = sb.ToString();
+            Console.WriteLine(texts);
         }
     }
 }
