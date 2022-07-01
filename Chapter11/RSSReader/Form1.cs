635de2341cc4a03fbb6692720eccf3bed26201c5
33282歩季　中山 <33282@ojs.ac.jp>
2022-07-01 14:05:33 +0900

    RSSReader url取得

--- Chapter11/RSSReader/Form1.cs
+++ Chapter11/RSSReader/Form1.cs
@@ -13,7 +13,7 @@ using System.Xml.Linq;
 namespace RSSReader {
     public partial class Form1 : Form {
 
-        IEnumerable<string> xTitle;
+        IEnumerable<string> xTitle,xLink;
 
         public Form1()
         {
@@ -30,7 +30,8 @@ private void btRssGet_Click(object sender, EventArgs e)
                 var xdoc = XDocument.Load(stream);
 
                 xTitle = xdoc.Root.Descendants("item").Select(x => (string)x.Element("title"));
-                
+                xLink = xdoc.Root.Descendants("item").Select(x => (string)x.Element("link"));
+
                 foreach (var data in xTitle)
                 {
                     lbRssTitle.Items.Add(data);
@@ -40,20 +41,11 @@ private void btRssGet_Click(object sender, EventArgs e)
 
         private void lbRssTitle_Click(object sender, EventArgs e)
         {
-            using (var wc = new WebClient())
-            {
-                int index = lbRssTitle.SelectedIndex;
-                var stream = wc.OpenRead(cbRssUrl.Text) ;
+            int index = lbRssTitle.SelectedIndex;
 
-                var xdoc = XDocument.Load(stream);
+            var url = xLink.ElementAt(index);
 
-
-                var xLink = xdoc.Root.Descendants("item").Select(x => x.Element("link"));
-                foreach (var data in xLink)
-                {
-                    wbBrowser.Navigate(data.Value);
-                }
-            }
+            wbBrowser.Navigate(url);
         }
 
         private void Form1_Load(object sender, EventArgs e)
