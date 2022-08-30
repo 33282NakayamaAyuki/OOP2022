7e04d8dd1c5af309d4350fec601b4ab5fd5f802d
33282歩季　中山 <33282@ojs.ac.jp>
2022-08-30 15:58:51 +0900

    演習問題17.2　完成

---
+++ Chapter17/Strategy/DistanceConverter/ConcreateConverter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+
+    public class MeterConverter : ConverterBase {
+        protected override double Ratio => 1;
+        public override string UnitName => "メートル";
+
+        public override bool IsMyUnit(string name)
+        {
+            return name.ToLower() == "meter" || name == UnitName;
+        }
+    }
+
+    public class FeetConverter : ConverterBase {
+        protected override double Ratio => 0.3048;
+        public override string UnitName => "フィート";
+        public override bool IsMyUnit(string name)
+        {
+            return name.ToLower() == "feet" || name == UnitName;
+        }
+    }
+
+    public class InchConverter : ConverterBase {
+        protected override double Ratio => 0.0254;
+        public override string UnitName => "インチ";
+        public override bool IsMyUnit(string name)
+        {
+            return name.ToLower() == "inch" || name == UnitName;
+        }
+    }
+
+    public class YardConverter : ConverterBase {
+        protected override double Ratio { get { return 0.9144; } }
+        public override string UnitName { get { return "ヤード"; } }
+        public override bool IsMyUnit(string name)
+        {
+            return name.ToLower() == "yard" || name == UnitName;
+        }
+    }
+    public class MileConverter : ConverterBase {
+        protected override double Ratio { get { return 1609.344; } }
+        public override string UnitName { get { return "マイル"; } }
+        public override bool IsMyUnit(string name)
+        {
+            return name.ToLower() == "mile" || name == UnitName;
+        }
+    }
+    public class KiloMeterConverter : ConverterBase {
+        protected override double Ratio { get { return 1000; } }
+        public override string UnitName { get { return "キロメートル"; } }
+        public override bool IsMyUnit(string name)
+        {
+            return name.ToLower() == "kilometer" || name == UnitName;
+        }
+    }
+}
