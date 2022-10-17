4b6ac1fff862e94fcda35c121cbb8ca7a69440f7
33282歩季　中山 <33282@ojs.ac.jp>
2022-10-17 14:17:27 +0900

    MainWindow 途中経過

--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -21,12 +21,48 @@ namespace SampleApplication {
         public MainWindow()
         {
             InitializeComponent();
+            getSeason();
         }
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("ボタンが押された");
         }
+
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            checkBoxTextBlock.Text = "チェック済み";
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            checkBoxTextBlock.Text = "未チェック";
+        }
+
+        private void redRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            colorTextBox.Text = "赤";
+        }
+
+        private void yellowRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            colorTextBox.Text = "黄";
+
+        }
+
+        private void blueRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            colorTextBox.Text = "青";
+
+        }
+
+        private void seasonComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            seasonTextBlock.Text = (string)((ComboBoxItem)seasonComboBox.SelectedItem).Content;
+        }
+
 
         private void okButton_Click(object sender, RoutedEventArgs e)
         {
@@ -37,5 +73,29 @@ namespace SampleApplication {
         {
 
         }
+
+        private void getSeason() {
+            int month = DateTime.Now.Month;
+            if (3 <= month && month <= 5)
+            {
+                seasonComboBox.SelectedIndex = 0;
+
+            } else if (6 <= month && month <= 8)
+            {
+                seasonComboBox.SelectedIndex = 1;
+
+            } else if (9 <= month && month <= 11)
+            {
+                seasonComboBox.SelectedIndex = 2;
+
+            } else if (month == 12 || month == 1 || month == 2)
+            {
+                seasonComboBox.SelectedIndex = 3;
+
+            } else {
+                seasonComboBox.SelectedIndex = 0;
+            }
+        }
+
     }
 }
