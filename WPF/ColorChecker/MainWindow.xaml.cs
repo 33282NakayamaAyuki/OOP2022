2a69e42ffb44f34e74acd6eb3fafa0b76f163704
33282歩季　中山 <33282@ojs.ac.jp>
2022-10-27 11:48:52 +0900

    Color Checker 10/27　午前分　完成

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,9 +23,19 @@ namespace ColorChecker {
         {
             InitializeComponent();
             LabelColor.Background = new SolidColorBrush(Color.FromRgb((byte)SliderRed.Value, (byte)SliderGreen.Value, (byte)SliderBlue.Value));
-            
+            DataContext = GetColorList();
         }
 
+        private MyColor[] GetColorList()
+        {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+        }
+
+        public class MyColor {
+            public Color Color { get; set; }
+            public string Name { get; set; }
+        }
 
         private void SliderRed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
@@ -41,5 +52,22 @@ namespace ColorChecker {
             LabelColor.Background = new SolidColorBrush(Color.FromRgb((byte)SliderRed.Value, (byte)SliderGreen.Value, (byte)SliderBlue.Value));
         }
 
+
+        private void Border_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var mycolor = (MyColor)((ComboBox)sender).SelectedItem;
+            var color = mycolor.Color;
+            var name = mycolor.Name;
+
+            LabelColor.Background = new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
+            TextBoxRed.Text = color.R.ToString();
+            TextBoxGreen.Text = color.G.ToString();
+            TextBoxBlue.Text = color.B.ToString();
+        }
     }
 }
