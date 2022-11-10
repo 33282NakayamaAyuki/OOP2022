fcfb4028aa6f3c9f5f70acafb82e5ea942587851
33282歩季　中山 <33282@ojs.ac.jp>
2022-11-10 10:34:58 +0900

    ColorChecker　完成

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -29,16 +29,8 @@ namespace ColorChecker {
         public MainWindow()
         {
             InitializeComponent();
-            LabelColor.Background = new SolidColorBrush(Color.FromRgb((byte)SliderRed.Value, (byte)SliderGreen.Value, (byte)SliderBlue.Value));
-            DataContext = GetColorList();
-        }
-
-        
-
-        private MyColor[] GetColorList()
-        {
-            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
-                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+            LabelColor.Background = new SolidColorBrush(Color.FromRgb((byte)SliderRed.Value, (byte)SliderGreen.Value, (byte)SliderBlue.Value));//BackGroundの初期化
+            DataContext = GetColorList();//ComboBoxに色情報を追加
         }
 
         public class MyColor {
@@ -47,21 +39,63 @@ namespace ColorChecker {
 
             public override string ToString()
             {
-                return $"R:{Color.R} G:{Color.G} B:{Color.B}";
+                return $"R:{Color.R} G:{Color.G} B:{Color.B}";//MyColorだけの書式を変える
             }
         }
 
-        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            LabelColor.Background = new SolidColorBrush(Color.FromRgb((byte)SliderRed.Value, (byte)SliderGreen.Value, (byte)SliderBlue.Value));
+        private class ColorInfo {
+            public SolidColorBrush Brush { get; set; }
+            public string Name { get; set; }
         }
 
+        private MyColor[] GetColorList()
+        {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();//Color情報を配列に
+        }
 
         private void Border_Loaded(object sender, RoutedEventArgs e)
         {
 
         }
+        
+        //stockListの色情報をLabelに追加
+        private void ButtonStack_Click(object sender, RoutedEventArgs e)
+        {
+            MyColor item = new MyColor { Color = Color.FromRgb((byte)SliderRed.Value, (byte)SliderGreen.Value, (byte)SliderBlue.Value) };
 
+            var colorName = ((IEnumerable<MyColor>)DataContext)
+                            .Where(c => c.Color.R == item.Color.R &&
+                                        c.Color.G == item.Color.G &&
+                                        c.Color.B == item.Color.B).FirstOrDefault();
+
+            stockList.Items.Add(colorName?.Name ?? "R:" + TextBoxRed.Text + " G:" + TextBoxGreen.Text + " B:" + TextBoxBlue);
+            //stockList.Items.Insert(0,colorName?.Name ?? "R:" + TextBoxRed.Text + " G:" + TextBoxGreen.Text + " B:" + TextBoxBlue);
+
+            colorList.Add(item);
+            //colorList.Insert(0,item);
+
+        }
+
+        //stockListの色情報を削除
+        private void ButtonRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if (stockList.SelectedItem != null)
+            {
+                int sel = stockList.SelectedIndex;
+                stockList.Items.RemoveAt(sel);
+                colorList.RemoveAt(sel);
+            }
+
+        }
+
+        //Sliderの値をLabelに
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            LabelColor.Background = new SolidColorBrush(Color.FromRgb((byte)SliderRed.Value, (byte)SliderGreen.Value, (byte)SliderBlue.Value));
+        }
+
+        //ComboBoxの色情報をLabelに
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             mycolor = (MyColor)((ComboBox)sender).SelectedItem;
@@ -73,26 +107,7 @@ namespace ColorChecker {
             TextBoxBlue.Text = color.B.ToString();
         }
 
-        private void ButtonStack_Click(object sender, RoutedEventArgs e)
-        {
-            MyColor item = new MyColor { Color = Color.FromRgb((byte)SliderRed.Value, (byte)SliderGreen.Value, (byte)SliderBlue.Value) };
-
-            var colorName = ((IEnumerable<MyColor>)DataContext)
-                            .Where(c => c.Color.R == item.Color.R &&
-                                        c.Color.G == item.Color.G &&
-                                        c.Color.B == item.Color.B).FirstOrDefault();
-
-            stockList.Items.Insert(0,colorName?.Name ?? "R:" + TextBoxRed.Text + " G:" + TextBoxGreen.Text + " B:" + TextBoxBlue);
-
-            colorList.Insert(0, item);
-
-        }
-
-        private class ColorInfo {
-            public SolidColorBrush Brush { get; set; }
-            public string Name { get; set; }
-        }
-
+        //stockListの色情報をLabelに
         private void stockList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (stockList.SelectedIndex != -1) {
@@ -102,13 +117,6 @@ namespace ColorChecker {
             }
         }
 
-        private void ButtonRemove_Click(object sender, RoutedEventArgs e)
-        {
-            if (stockList.SelectedItem != null)
-            {
-                int sel = stockList.SelectedIndex;
-                stockList.Items.RemoveAt(sel);
-            }
-        }
+
     }
 }
